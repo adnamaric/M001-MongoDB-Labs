53dde09c43dde4943df1de0f1b22fb9a67c13e57
Adna Marić <55598264+adnamaric@users.noreply.github.com>
2022-01-26 23:59:35 +0100

    Lab1.js --corrections

--- M103 - Basic Cluster Administration/Chapter 3 - Sharding/Lab 1 - Deploy a Sharded Cluster/Lab1.js
+++ M103 - Basic Cluster Administration/Chapter 3 - Sharding/Lab 1 - Deploy a Sharded Cluster/Lab1.js
@@ -18,9 +18,3 @@ Solution:
     mongo --port 26000  -u "m103-admin" -password "m103-pass"
     sh.addShard("shard1/localhost:27001")
 
-    mongoimport /dataset/products.json --port 26000  --db "m103" --collection  "products" -u "m103-admin" -p "m103-pass" --authenticationDatabase admin
-    mongo --port 26000 -u "m103-admin" -p "m103-pass"
-    use m103
-    sh.enableSharding("m103")
-    db.products.createIndex( { "sku": 1 } )
-    sh.shardCollection( "m103.products", { "sku": 1 } )
